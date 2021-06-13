44031def081cb7349bb410a20a6f19acf9ff9c19
Romain KOENIG <romain.koenig@gmail.com>
2021-06-13 23:44:50 +0200

    Getting real data from LA SERRE and printing it

--- index.js
+++ index.js
@@ -9,3 +9,29 @@ require('dotenv').config();
 const profile = process.env.SERRE_PROFILE_ID;
 
 logging(profile);
+
+
+
+
+// REAL TREATMENT STARTS HERE
+
+(async () => {
+
+	try {
+		let response = await fetch(`https://serre.grainesdesol.fr/wp-json/buddypress/v1/members/?search=${profile}`, {
+			method: 'GET'
+		});
+
+		logging(response);
+
+		const profileInfo = await response.text();
+
+		logging(profileInfo);
+
+		
+	}
+	catch (error) {
+		console.error(error);
+	}
+
+})();
